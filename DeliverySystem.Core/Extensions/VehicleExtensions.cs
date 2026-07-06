a0d5ea51db7d029097dbf6541949706f64281eb9
pajm <pajmaher@gmail.com>
2026-07-06 11:40:37 +0100

    Added extension methods

---
+++ DeliverySystem.Core/Extensions/VehicleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DeliverySystem.Core.Models;
+
+namespace ExtensionMethods.Core.Extensions
+{
+    public static class VehicleExtensions
+    {
+        public static bool IsOverloaded(this Vehicle v)
+        {
+            var currentWeight = v.GetDeliveries().Sum(d => d.WeightKg);
+            if (currentWeight > v.MaxPayloadKg) return true;
+            return false;
+        }
+
+        public static decimal AvailablePayload(this Vehicle v)
+        {
+            var currentWeight = v.GetDeliveries().Sum(d => d.WeightKg);
+            return v.MaxPayloadKg - currentWeight;
+        }
+    }
+}
